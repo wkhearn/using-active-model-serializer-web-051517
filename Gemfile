851fffe6ad5ca7951a1f15f6fdb70031dbe08099
wkhearn <wkhearn@gmail.com>
2017-07-11 00:32:30 -0400

    Done.

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'active_model_serializers'
 
 group :development, :test do
   gem 'pry-byebug'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     respond_to do |format|
       format.html { render :show }
-      format.json { render json: @post.to_json(only: [:title, :description, :id],
-                              include: [author: { only: [:name]}]) }
+      format.json { render json: @post }
     end
   end
 
